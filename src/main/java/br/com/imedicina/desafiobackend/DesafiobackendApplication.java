555b027295bc61d5f8fd5e000c0aef9caedba8ed
lucianobritodev <lucianobrito.dev@gmail.com>
2023-01-06 15:33:14 -0300

    Created project

---
+++ src/main/java/br/com/imedicina/desafiobackend/DesafiobackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.imedicina.desafiobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiobackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/imedicina/desafiobackend/DesafiobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.imedicina.desafiobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
