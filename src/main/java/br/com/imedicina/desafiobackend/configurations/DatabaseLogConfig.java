4fa4858cb98add9cd293cadf68dd2a0b63e6a75e
lucianobritodev <lucianobrito.dev@gmail.com>
2023-01-16 17:03:36 -0300

    Added Database configuration

--- src/main/java/br/com/imedicina/desafiobackend/configurations/DatabaseLogConfig.java
+++ src/main/java/br/com/imedicina/desafiobackend/configurations/DatabaseLogConfig.java
@@ -1,9 +1,22 @@
 package br.com.imedicina.desafiobackend.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.time.ZonedDateTime;
+import java.util.Optional;
 
 @Configuration
+@EnableTransactionManagement
 @EnableJpaAuditing
 public class DatabaseLogConfig {
+
+    @Bean
+    public DateTimeProvider auditingDateTimeProvider() {
+        return () -> Optional.of(ZonedDateTime.now());
+    }
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,21 @@
 
 insert into patient
-    (id, created_at, last_modified_at, online_status, password, total_appointment, username)
+    (id, date_created, date_modified, online_status, password, total_appointment, username)
 values
     ((select nextval('seq_patient')), now(), now(), true, '$2a$10$uqEVq2J6iPBoPGrZPkcNEue6lQZ2bf0iPo1ix/va.n9.x7UcwG8/S', 0, 'admin'),
     ((select nextval('seq_patient')), now(), now(), true, '$2a$10$uqEVq2J6iPBoPGrZPkcNEue6lQZ2bf0iPo1ix/va.n9.x7UcwG8/S', 0, 'user-test'),
     ((select nextval('seq_patient')), now(), now(), true, '$2a$10$uqEVq2J6iPBoPGrZPkcNEue6lQZ2bf0iPo1ix/va.n9.x7UcwG8/S', 10, 'luciano');
+
+insert into health_insurance
+(id, date_created, date_modified, price, cnpj, name)
+values
+    ((select nextval('seq_health_insurance')), now(), now(), 199.90, '27655133000152', 'Breath Health'),
+    ((select nextval('seq_health_insurance')), now(), now(), 299.90, '50736355000111', 'Live Heath Insurance'),
+    ((select nextval('seq_health_insurance')), now(), now(), 599.90, '29357738000192', 'Living');
+
+insert into healthcare_professional
+(id, date_created, date_modified, crm, rating, status, name)
+values
+    ((select nextval('seq_healthcare_professional')), now(), now(), 59381, 5, true, 'Maria'),
+    ((select nextval('seq_healthcare_professional')), now(), now(), 63854, 4, false, 'José'),
+    ((select nextval('seq_healthcare_professional')), now(), now(), 31852, 4, true, 'Pedro');
